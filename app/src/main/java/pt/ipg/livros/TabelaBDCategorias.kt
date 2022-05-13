10544cdc00a296e3b049eb1926b39038d28d939d
Noel Lopes <noel@ipg.pt>
2022-05-13 12:09:51 +0100

    Criada classe para representar a tabela de categorias (com a função cria para criar a tabela)

---
+++ app/src/main/java/pt/ipg/livros/TabelaBDCategorias.kt
@@ -0,0 +1,15 @@
+package pt.ipg.livros
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDCategorias(val db: SQLiteDatabase) {
+    fun cria() {
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL)")
+    }
+
+    companion object {
+        const val NOME = "categorias"
+        const val CAMPO_NOME = "nome"
+    }
+}
