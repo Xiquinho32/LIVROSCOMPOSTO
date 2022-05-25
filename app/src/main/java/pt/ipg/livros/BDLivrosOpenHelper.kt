831bf7a6d2030b1523f22d9a36458a10896ca40e
Noel Lopes <noel@ipg.pt>
2022-05-25 11:19:09 +0100

    Simplificar construtor da classe BDLivrosOpenHelper

--- app/src/main/java/pt/ipg/livros/BDLivrosOpenHelper.kt
+++ app/src/main/java/pt/ipg/livros/BDLivrosOpenHelper.kt
@@ -4,12 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class BDLivrosOpenHelper(
-    context: Context?,
-    name: String?,
-    factory: SQLiteDatabase.CursorFactory?,
-    version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
+class BDLivrosOpenHelper(context: Context?) : SQLiteOpenHelper(context, NOME, null, VERSAO) {
     /**
      * Called when the database is created for the first time. This is where the
      * creation of tables and the initial population of the tables should happen.
@@ -46,5 +41,10 @@ class BDLivrosOpenHelper(
      * @param newVersion The new database version.
      */
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    companion object {
+        const val NOME = "livros.db"
+        private const val VERSAO = 1
     }
 }
