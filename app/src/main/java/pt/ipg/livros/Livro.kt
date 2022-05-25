4c3ca0302eeeeaadbf3ee99e0e3b6d9060961324
Noel Lopes <noel@ipg.pt>
2022-05-25 12:35:16 +0100

    Criada função para converter uma Livro em ContentValues

--- app/src/main/java/pt/ipg/livros/Livro.kt
+++ app/src/main/java/pt/ipg/livros/Livro.kt
@@ -1,9 +1,20 @@
 package pt.ipg.livros
 
+import android.content.ContentValues
+
 data class Livro(
     var id: Long,
     var titulo : String,
     var autor: String,
     var idCategoria: Long
 ) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDLivros.CAMPO_TITULO, titulo)
+        valores.put(TabelaBDLivros.CAMPO_AUTOR, autor)
+        valores.put(TabelaBDLivros.CAMPO_CATEGORIA_ID, idCategoria)
+
+        return valores
+    }
 }
