6115d5a2da16eb689d064a63c060abe9b171ada1
Noel Lopes <noel@ipg.pt>
2022-05-25 12:33:10 +0100

    Criada função para converter uma Categoria em ContentValues

--- app/src/main/java/pt/ipg/livros/Categoria.kt
+++ app/src/main/java/pt/ipg/livros/Categoria.kt
@@ -1,4 +1,13 @@
 package pt.ipg.livros
 
+import android.content.ContentValues
+import android.provider.BaseColumns
+
 data class Categoria(var id: Long, var nome: String) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+        valores.put(TabelaBDCategorias.CAMPO_NOME, nome)
+
+        return valores
+    }
 }
