2cd02b37d3633cdc8593d219cd899f64c61252e5
Xiquinho32 <franciscofcp32@gmail.com>
2022-06-08 10:17:31 +0100

    criada uriMatcher para o ContentProviderLivros.kt

--- app/src/main/java/pt/ipg/livros/ContentProviderLivros.kt
+++ app/src/main/java/pt/ipg/livros/ContentProviderLivros.kt
@@ -2,6 +2,7 @@ package pt.ipg.livros
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.database.sqlite.SQLiteOpenHelper
 import android.net.Uri
@@ -38,5 +39,25 @@ class ContentProviderLivros: ContentProvider() {
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
+    }
+    companion object{
+        const val AUTORIDADE = "pt.ipg.livros"
+        const val URI_CATEGORIAS = 100
+        const val URI_LIVROS = 200
+
+        const val URI_CATEGORIAS_ESPECIFICA = 101
+        const val URI_LIVROS_ESPECIFICA = 201
+
+        fun getUriMatcher() : UriMatcher{
+            val uriMatcher = UriMatcher(UriMatcher.NO_MATCH) //nao existe correspondência
+
+            uriMatcher.addURI(AUTORIDADE, TabelaBDCategorias.NOME, URI_CATEGORIAS)//add endereços
+            uriMatcher.addURI(AUTORIDADE, TabelaBDLivros.NOME, URI_LIVROS)//add endereços
+
+            uriMatcher.addURI(AUTORIDADE,"${TabelaBDCategorias.NOME}/#", URI_CATEGORIAS_ESPECIFICA)//add endereços com uma categoria especifica
+            uriMatcher.addURI(AUTORIDADE,"${TabelaBDLivros.NOME}/#", URI_LIVROS_ESPECIFICA)
+
+            return uriMatcher
+        }
     }
 }
